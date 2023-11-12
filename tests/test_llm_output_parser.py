55c48699ebdcf3672f8a289247a95ca65eb0afd4
Jurgen Strydom <jurgen.strydom@gmail.com>
2023-11-12 13:42:17 +0200

    tests, that log on failure, and runs over all data

---
+++ tests/test_llm_output_parser.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+if __name__ == '__main__':
+    parentdir = os.path.dirname(currentdir)
+    sys.path.insert(0, parentdir)
+    data_dir = os.path.join(parentdir, 'data')
+else:
+    data_dir = os.path.join(currentdir, 'data')
+
+import unittest
+import pandas as pd
+from llm_output_parser import extract_tasks_content
+from response_components import project_plan_components, task_outline_components
+import re
+
+from unittestlogging import TestCaseLogging
+
+class TestProjectPlan(TestCaseLogging):
+
+    def setUp(self):
+        self.setup_logging(log_failures=True, log_successes=False, filename='test_llm_output_parser.log')
+
+    def run_on_all_project_plan_data(func):
+        def wrapper(self):
+            print(os.path.join(data_dir, 'project_plan.parquet'))
+            df = pd.read_parquet(os.path.join(data_dir, 'project_plan.parquet'))
+            for index, row in df.iterrows():
+                    func(self, row.iloc[0])
+        return wrapper
+
+    @run_on_all_project_plan_data
+    def test_extract_tasks(self, data):
+        extracted_data = extract_tasks_content(data, project_plan_components)
+        self.assertIn('tasks', extracted_data.keys())
+        if 'tasks' in extracted_data.keys():
+            tasks_data = extracted_data['tasks']
+            self.assertEqual(type(tasks_data), list)
+            for li in tasks_data:
+                self.assertEqual(type(li), str)
+                if isinstance(li, str):
+                    li_no_number = re.search(r'^\d+(?:\.\d+)*\.\s*(.*)', li).group(1)
+                    self.assertIn(li_no_number, data)
+                    
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unittestlogging.py
@@ -0,0 +1,41 @@
+import unittest
+import logging
+import traceback
+
+class TestCaseLogging(unittest.TestCase):
+    def __init__(self, *args):
+        super(TestCaseLogging, self).__init__(*args)
+        for name, method in unittest.TestCase.__dict__.items():
+            if callable(method) and name.startswith('assert') and ('_' not in name):
+                setattr(unittest.TestCase, name, self.log_data(method))
+
+    @staticmethod
+    def get_traceback():
+        return '\n'.join([a for a in traceback.format_exc().split('\n') if 'unittestlogging.py' not in a])
+
+    def log_all_arguments(self, args):
+        for arg in args:
+            logging.warning(arg)
+
+    def log_data(self, func):
+        def wrapper(self, *args):
+            try:
+                func(self, *args)
+                if self.log_successes:
+                    logging.error("SUCCESS")
+                    self.log_all_arguments(args)
+            except Exception as e:
+                if self.log_failures:
+                    logging.error("FAILURE")
+                    self.log_all_arguments(args)
+                    logging.error(self.get_traceback())
+                    
+                # raise
+        return wrapper
+
+    def setup_logging(self, log_failures:bool=True, log_successes:bool=False, filename:str = 'logs.log', level=logging.DEBUG):
+        self.log_failures = log_failures
+        self.log_successes = log_successes
+        logging.basicConfig(filename=filename, encoding='utf-8', level=level, filemode='w',
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
